6720f873866dc2b79a53bfe4bae52427db2c32df
Live <nkline@solarinnovations.com>
2017-11-22 16:13:29 -0500

    layer slider update

--- wp-content/plugins/LayerSlider/wp/hooks.php
+++ wp-content/plugins/LayerSlider/wp/hooks.php
@@ -96,6 +96,7 @@ function ls_parse_defaults($defaults = array(), $raw = array()) {
 		// Check premium features
 		$isPremium = false;
 		if( ! empty( $default['premium'] ) && ! $activated ) {
+
 			if( ! $permission ) {
 
 				if( ! empty( $raw['styles'][$phpKey] ) ) {
@@ -147,7 +148,15 @@ function ls_parse_defaults($defaults = array(), $raw = array()) {
 		$premiumStyle = false;
 		if( $isPremium && ! empty( $raw['styles'][$phpKey] ) ) {
 			if( (string)$default['value'] !== (string)$raw['styles'][$jsKey] ) {
-				$premiumStyle = true;
+
+				// v6.6.4: Fix blend-mode due to change in default value
+				if( $phpKey === 'mix-blend-mode' && $raw['styles'][$phpKey] === 'normal' ) {
+					// Do nothing, the 'normal' blend-mode value should not be
+					// counted as
+
+				} else {
+					$premiumStyle = true;
+				}
 			}
 		}
 
