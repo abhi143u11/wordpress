160c6e59173b413e1bbbb2e78d1db8a2feaed9b8
Nicholas Kline <ndkline@theagora.us>
2018-05-11 08:11:27 -0400

    merge fix

--- wp-content/themes/solar-innovations/ultimate-member/email/notification_new_user.php
+++ wp-content/themes/solar-innovations/ultimate-member/email/notification_new_user.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 {display_name} has just created an account on {site_name}. To view their profile click here:
 
 {user_profile_link}
@@ -6,6 +5,3 @@
 Here is the submitted registration form:
 
 {submitted_registration}
-=======
-<p>{display_name} has just created an account on {site_name}. To view their profile click here: {user_profile_link} Here is the submitted registration form: {submitted_registration}</p>
->>>>>>> 01cd3400df28de7997230e7b4299d723a1154df5
